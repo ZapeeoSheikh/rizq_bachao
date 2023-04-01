8713a6275524f9d95e221fac8051ecbc518829fb
ZapeeoSheikh <muhammadrmz871@gmail.com>
2023-04-02 01:03:58 +0500

    home/forum

--- lib/Pages/BottomNavigationPages/forum.dart
+++ lib/Pages/BottomNavigationPages/forum.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../Ui Kit/ui.dart';
 
 class Forum extends StatefulWidget {
   const Forum({Key? key}) : super(key: key);
@@ -10,6 +14,135 @@ class Forum extends StatefulWidget {
 class _ForumState extends State<Forum> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 120.h,
+        backgroundColor: MyColor.bottomNavigationBar,
+        title: Row(
+          children: [
+            Column(
+              children: [
+                Text(
+                  "Forum",
+                  style: GoogleFonts.roboto(
+                    fontSize: 24.sp,
+                    color: MyColor.textColor1,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: MyColor.cardBack,
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30.0, vertical: 9),
+                    child: Row(
+                      children: [
+                        ImageIcon(
+                          AssetImage(
+                            "assets/icons/filter.png",
+                          ),
+                          color: MyColor.textColor1,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "Category",
+                          style: GoogleFonts.roboto(
+                            fontSize: 16.sp,
+                            color: MyColor.textColor1,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.search,
+            ),
+            color: MyColor.textColor1,
+            onPressed: () {},
+          )
+        ],
+        automaticallyImplyLeading: false,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(25),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      child: Card(
+                        child: Row(
+                          children: [
+                            Row(
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.all(Radius.circular(100)),
+                                  child: Image(
+                                    image:
+                                    AssetImage("assets/Vector/avatar.png"),
+                                    width: MediaQuery.of(context).size.width * .27,
+                                    height: 80.sp,
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 14.w,
+                                ),
+                                Row(
+                                  children: [
+                                    Column(
+                                      children: [
+                                        Container(
+                                          width:
+                                          MediaQuery.of(context).size.width / 1.3,
+                                          child: Text(
+                                            "Sara posted in Spreading The world",
+                                            style: GoogleFonts.roboto(
+                                              fontSize: 16.sp,
+                                              color: MyColor.textColor1,
+                                            ),
+                                          ),
+                                        ),
+                                        Row(
+                                          children: [
+                                            Icon(Icons.access_time_outlined, size: 10, color: MyColor.inActive,)
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              width: 10.w,
+                            ),
+
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/Pages/BottomNavigationPages/home.dart
+++ lib/Pages/BottomNavigationPages/home.dart
@@ -1,11 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:rizq_bachao/Ui%20Kit/ui.dart';
 
-import 'forum.dart';
-import 'goals.dart';
-import 'message.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -15,6 +13,12 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  bool isheart = false;
+
+  int count = 6;
+
+  bool isvisible = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -84,92 +88,107 @@ class _HomeState extends State<Home> {
           padding: const EdgeInsets.all(25.0),
           child: Column(
             children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          boxShadow: [BoxShadow(
-                              offset: Offset.fromDirection(2),
-                              blurRadius: 20,
-                              spreadRadius: 3,
-                              color: MyColor.inActive
-                          )],
-                          borderRadius: BorderRadius.circular(20)),
-                      child: Padding(
-                        padding: const EdgeInsets.all(22.0),
-                        child: Column(
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Image(
-                                    image: AssetImage(
-                                        "assets/Vector/loudspeaker.png")),
-                                Text(
-                                  "Spread the world!",
-                                  style: GoogleFonts.roboto(
-                                    fontSize: 24.sp,
-                                    color: MyColor.textColor1,
+              
+              Visibility(
+                visible: isvisible ? true : false,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                  offset: Offset.fromDirection(2),
+                                  blurRadius: 20,
+                                  spreadRadius: 3,
+                                  color: MyColor.inActive)
+                            ],
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(22.0),
+                          child: Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Image(
+                                      image: AssetImage(
+                                          "assets/Vector/loudspeaker.png")),
+                                  Text(
+                                    "Spread the world!",
+                                    style: GoogleFonts.roboto(
+                                      fontSize: 24.sp,
+                                      color: MyColor.textColor1,
+                                    ),
                                   ),
-                                ),
-                                Container(
-                                  decoration: BoxDecoration(
-                                      color: MyColor.iconActive,
-                                      borderRadius: BorderRadius.circular(100)),
-                                  child: Icon(
-                                    Icons.close,
-                                    color: Colors.white,
-                                  ),
-                                )
-                              ],
-                            ),
-                            SizedBox(
-                              height: 17.h,
-                            ),
-                            RichText(
-                              text: TextSpan(
-                                text:
-                                    'Wishes there were more items near you? Help spread the world about Rizq Bachao! ',
-                                style: GoogleFonts.roboto(
-                                  fontSize: 16.sp,
-                                  color: MyColor.iconActive,
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 16.h,
-                            ),
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: Container(
-                                    height: 50.h,
-                                    decoration: BoxDecoration(
-                                        color: MyColor.mainColor1,
-                                        borderRadius: BorderRadius.circular(10)),
-                                  child: Center(
-                                    child: Text(
-                                      "Find out more",
-                                      style: GoogleFonts.roboto(
-                                        fontSize: 16.sp,
+                                  GestureDetector(
+                                    onTap: (){
+                                      setState(() {
+                                        isvisible = false;
+                                      });
+                                    },
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                          color: MyColor.iconActive,
+                                          borderRadius: BorderRadius.circular(100)),
+                                      child: Icon(
+                                        Icons.close,
                                         color: Colors.white,
                                       ),
                                     ),
-                                  ),
+                                  )
+                                ],
+                              ),
+                              SizedBox(
+                                height: 17.h,
+                              ),
+                              RichText(
+                                text: TextSpan(
+                                  text:
+                                      'Wishes there were more items near you? Help spread the world about Rizq Bachao! ',
+                                  style: GoogleFonts.roboto(
+                                    fontSize: 16.sp,
+                                    color: MyColor.iconActive,
                                   ),
                                 ),
-                              ],
-                            )
-                          ],
+                              ),
+                              SizedBox(
+                                height: 16.h,
+                              ),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: Container(
+                                      height: 50.h,
+                                      decoration: BoxDecoration(
+                                          color: MyColor.mainColor1,
+                                          borderRadius:
+                                              BorderRadius.circular(10)),
+                                      child: Center(
+                                        child: Text(
+                                          "Find out more",
+                                          style: GoogleFonts.roboto(
+                                            fontSize: 16.sp,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-              SizedBox(height: 14.h,),
+              SizedBox(
+                height: 14.h,
+              ),
               Row(
                 children: [
                   Text(
@@ -181,8 +200,134 @@ class _HomeState extends State<Home> {
                   ),
                 ],
               ),
-              SizedBox(height: 14.h,),
-Card()
+              SizedBox(
+                height: 14.h,
+              ),
+              for(int i = 0; i <= 20; i++)
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      child: Card(
+                        child: Row(
+                          children: [
+                            Image(
+                              image:
+                                  AssetImage("assets/Vector/hungery_girl.png"),
+                              width: MediaQuery.of(context).size.width * .27,
+                              height: 130.sp,
+                            ),
+                            SizedBox(
+                              width: 10.w,
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    Container(
+                                      width:
+                                          MediaQuery.of(context).size.width / 4,
+                                      child: Text(
+                                        "Mixed Salad Sara",
+                                        style: GoogleFonts.roboto(
+                                          fontSize: 16.sp,
+                                          color: MyColor.textColor1,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 4.w,
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      "41 minutes ago",
+                                      style: GoogleFonts.roboto(
+                                        fontSize: 12.sp,
+                                        color: MyColor.inActive,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 4.w,
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      children: [
+                                        for (var i = 0; i <= 4; i++)
+                                          Icon(
+                                            Icons.star,
+                                            color: CupertinoColors.systemYellow,
+                                            size: 16,
+                                          ),
+                                      ],
+                                    ),
+                                   SizedBox(width: 15,),
+                                   GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          if(isheart){
+                                            isheart = false;
+                                            count = count-1;
+                                          }
+                                          else {
+                                            isheart = true;
+                                            count = count+1;
+                                          }
+                                        });
+                                      },
+                                      child: Icon(
+                                        isheart ? Icons.favorite : Icons.favorite_border,
+                                        color: MyColor.mainColor1,
+                                        size: 16,
+                                      ),
+                                    ),
+                                    SizedBox(width: 10,),
+                                    Text(
+                                      "${count}",
+                                      style: GoogleFonts.roboto(
+                                        fontSize: 12.sp,
+                                        color: MyColor.inActive,
+                                      ),
+                                    ),
+                                    SizedBox(width: 80,),
+                                    GestureDetector(
+                                      onTap: () {
+                                        setState(() {
+                                          if(isheart){
+                                            isheart = false;
+                                            count = count-1;
+                                          }
+                                          else {
+                                            isheart = true;
+                                            count = count+1;
+                                          }
+                                        });
+                                      },
+                                      child: Icon(
+                                        isheart ? Icons.remove_red_eye : Icons.close,
+                                        color: MyColor.mainColor1,
+                                        size: 16,
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),

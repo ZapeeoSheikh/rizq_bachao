2d49b5f3164928325959e6906b0855f81664ef73
ZapeeoSheikh <muhammadrmz871@gmail.com>
2023-04-04 10:35:40 +0500

    documentation

--- README.md
+++ README.md
@@ -1,16 +1,37 @@
-# rizq_bachao
+# Flutter Project
 
-A new Flutter project.
+This is a Flutter project for creating a mobile application. Flutter is an open-source UI software development kit created by Google. It is used to develop applications for Android, iOS, Linux, Mac, Windows, Google Fuchsia, and the web.
 
-## Getting Started
+## Requirements
 
-This project is a starting point for a Flutter application.
+- Flutter SDK
+- Android Studio
+- Xcode
 
-A few resources to get you started if this is your first Flutter project:
+## Installation
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+1. Download and install the Flutter SDK from the [Flutter website](https://flutter.dev/). Ensure that you install the latest stable version of the Flutter SDK.
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+2. Download and install Android Studio from the [Android Studio website](https://developer.android.com/studio). Ensure that you install the latest stable version of Android Studio.
+
+3. Download and install Xcode from the [Apple App Store](https://apps.apple.com/us/app/xcode/id497799835). Ensure that you install the latest stable version of Xcode.
+
+## Usage
+
+1. Open Android Studio and select "Open an existing Android Studio project".
+
+2. Navigate to the directory containing your Flutter project and select the `android` directory.
+
+3. Open Xcode and select "Create a new Xcode project".
+
+4. Navigate to the directory containing your Flutter project and select the `ios` directory.
+
+5. In both Android Studio and Xcode, you can edit the project files to customize the application.
+
+6. When you are finished making changes, you can build the application for each platform by running the `flutter build` command in your terminal.
+
+7. Once you have built the application, you can deploy it to the app store of your choice.
+
+## Troubleshooting
+
+If you encounter any errors while building or deploying the application, you can refer to the [Flutter documentation](https://flutter.dev/docs) or [Stack Overflow](https://stackoverflow.com/) for help.
--- lib/Pages/BottomNavigationPages/message.dart
+++ lib/Pages/BottomNavigationPages/message.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../Ui Kit/ui.dart';
 
 class Message extends StatefulWidget {
   const Message({Key? key}) : super(key: key);
@@ -10,6 +14,89 @@ class Message extends StatefulWidget {
 class _MessageState extends State<Message> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        // toolbarHeight: 120.h,
+        backgroundColor: MyColor.bottomNavigationBar,
+        centerTitle: false,
+        title: Text(
+          "Message",
+          style: GoogleFonts.roboto(
+            fontSize: 24.sp,
+            color: MyColor.textColor1,
+          ),
+        ),
+        automaticallyImplyLeading: false,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(50)
+                ),
+                prefixIcon: Icon(
+                  Icons.search,
+                ),
+                hintText: "Search Message ..."
+              ),
+            ),
+            SizedBox(height: 15.h,),
+            Row(
+              children: [
+                Text(
+                  "Frequently contacted",
+                  style: GoogleFonts.roboto(
+                    fontSize: 20.sp,
+                    color:
+                    MyColor.textColor1,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20.h,),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  for(int i = 0; i<=20; i++)
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.blueAccent,
+                              boxShadow: [
+                                BoxShadow(color: MyColor.inActive, spreadRadius: 1, blurRadius: 8)
+                              ],
+                              borderRadius: BorderRadius.circular(100)
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius
+                                .circular(100
+                                .r), // Image border
+                            child:
+                            SizedBox.fromSize(
+                              size: Size.fromRadius(
+                                  30.r), // Image radius
+                              child: Image(
+                                image: AssetImage(
+                                    "assets/Vector/avatar.png"),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                  )
+
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
